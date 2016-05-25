8d8663bc4f6d521164e8510c6f3ef1864f430627
Elton Yang <eyang03@calpoly.edu>
2016-05-25 12:38:14 -0700

    updated disease to have UI and output. Ontology may be changed?

--- 481DxDz/src/main/java/cz/makub/Disease.java
+++ 481DxDz/src/main/java/cz/makub/Disease.java
@@ -17,10 +17,12 @@ import org.semanticweb.owlapi.formats.OWLXMLDocumentFormat;
 import org.semanticweb.owlapi.io.OWLObjectRenderer;
 import org.semanticweb.owlapi.model.*;
 import org.semanticweb.owlapi.model.parameters.Imports;
+import org.semanticweb.owlapi.reasoner.Node;
 import org.semanticweb.owlapi.reasoner.OWLReasoner;
 import org.semanticweb.owlapi.reasoner.OWLReasonerFactory;
 import org.semanticweb.owlapi.reasoner.SimpleConfiguration;
 import org.semanticweb.owlapi.search.EntitySearcher;
+import org.semanticweb.owlapi.util.DefaultPrefixManager;
 import org.semanticweb.owlapi.vocab.OWLRDFVocabulary;
 import uk.ac.manchester.cs.owl.explanation.ordering.ExplanationOrderer;
 import uk.ac.manchester.cs.owl.explanation.ordering.ExplanationOrdererImpl;
@@ -28,6 +30,7 @@ import uk.ac.manchester.cs.owl.explanation.ordering.ExplanationTree;
 import uk.ac.manchester.cs.owl.explanation.ordering.Tree;
 
 import java.util.*;
+import java.util.Map.Entry;
 import java.io.*;
 
 /**
@@ -39,9 +42,28 @@ import java.io.*;
  */
 public class Disease {
     private static final String BASE_URL = "http://users.csc.calpoly.edu/~eyang03/CPE481-KB/481DxDz/Ontology/Disease.owl";
-    //private static final String BASE_URL = "file:///C:/Users/Ginger/Documents/home/cpe481/Project/481DxDz/Ontology/disease.owl";
     //DL = Description Logic
     private static OWLObjectRenderer renderer = new DLSyntaxObjectRenderer();
+    // diseaseSymptoms = disease -> # of symptoms triggering disease
+    private static HashMap<String, Double> diseaseSymptoms = new HashMap<String, Double>();
+    
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+	private static LinkedList<Map.Entry<String, Double>> sortDiseases() {
+    	LinkedList<Map.Entry<String, Double>> list = new LinkedList<Map.Entry<String, Double>>(diseaseSymptoms.entrySet());
+        Collections.sort(list, new Comparator() {
+             public int compare(Object o1, Object o2) {
+                return ((Map.Entry<String, Double>)o2).getValue().compareTo(((Map.Entry<String, Double>)o1).getValue());
+             }
+        });
+        return list;
+    }
+    
+    private static OWLNamedIndividual createIndividual(OWLOntology ontology, PrefixDocumentFormat pm, OWLOntologyManager manager, String name) {
+        OWLDataFactory factory = manager.getOWLDataFactory();
+        OWLNamedIndividual individual = factory.getOWLNamedIndividual(name, pm);
+        manager.addAxiom(ontology, factory.getOWLDeclarationAxiom(individual));
+        return individual;
+    }
     
     public static void main(String[] args) throws OWLOntologyCreationException {
         System.out.println(BASE_URL);
@@ -53,26 +75,98 @@ public class Disease {
         OWLDataFactory factory = manager.getOWLDataFactory();
         PrefixDocumentFormat pm = manager.getOntologyFormat(ontology).asPrefixOWLOntologyFormat();
         pm.setDefaultPrefix(BASE_URL + "#");
-
+        HashMap<String, OWLNamedIndividual> symptomDB = new HashMap<String, OWLNamedIndividual>();
+        Scanner scan = new Scanner(System.in);
+        boolean selected[];
+        
         //get class and its individuals
         OWLClass diseaseClass = factory.getOWLClass(":Disease", pm);
-
-        for (OWLNamedIndividual disease : reasoner.getInstances(diseaseClass, false).getFlattened()) {
-            System.out.println("disease : " + renderer.render(disease));
+        OWLClass symptomClass = factory.getOWLClass(":Symptom", pm);
+        OWLClass patientClass = factory.getOWLClass(":Patient", pm);
+        
+        OWLNamedIndividual patient0 = createIndividual(ontology, pm, manager, ":patient0");
+        OWLObjectProperty hasSymptoms = factory.getOWLObjectProperty(":hasSymptoms", pm);
+        manager.addAxiom(ontology, factory.getOWLClassAssertionAxiom(patientClass, patient0));
+        
+        // create local symptom db for prompt
+        for (OWLNamedIndividual symptom : reasoner.getInstances(symptomClass, false).getFlattened()) {
+            symptomDB.put(renderer.render(symptom), symptom);
         }
+        
+        // prompt user for disease. Do we ask for multiple people, or just run them once?
+        // should we just keep it single user?
+        String[] symptoms =symptomDB.keySet().toArray(new String[0]);
+        selected = new boolean[symptomDB.size()];
+        
+        int longest = 0;
+        for (String str: symptoms)
+        	if (longest < str.length())
+        		longest = str.length();
+		longest++;
+        
+        String parse;
+        do {
+        	for (int i = 0; i < symptoms.length; i++) {
+        		if ((i != 0 && symptoms.length < 10) || (i % 3 == 0 && i != 0))
+        			System.out.println();
+        		int maxIntLength = (new String(""+symptoms.length)).length();
+        		String index = new String("" + (i+1));
+        		System.out.print("[" + (selected[i]? "x" : " ") + "] " +  String.format("%"+maxIntLength+"s", index) + ". " + symptoms[i].replace("_", " ") +  String.format("%" + (longest-symptoms[i].length()) + "s"," ") + " ");
+        	}
+        	System.out.print("\nPlease select symptoms (press <ENTER> to continue): ");
+        	parse = scan.nextLine().trim();
+        	if (parse.length() == 0) break;
+    		String[] parsed = parse.split(",");
+    		for (int i = 0; i < parsed.length; i++) {
+    			// no type checking
+    			selected[Integer.parseInt(parsed[i].trim())-1] = true;
+    		}
+        } while (true);
+        
+        // transfer symptoms into ontology
+		for (int i = 0; i < symptoms.length; i++) {
+			if (selected[i]) {
+//				System.out.print(symptoms[i]);
+        		symptomDB.get(symptoms[i]);
+//				System.out.println();
+			}
+		}
 
-        //get a given individual
-        OWLNamedIndividual flu = factory.getOWLNamedIndividual(":Flu", pm);
-
-        //get values of selected properties on the individual
-        OWLDataProperty includesSymptomProperty = factory.getOWLDataProperty(":includesSymptom", pm);
-
-        //OWLObjectProperty isEmployedAtProperty = factory.getOWLObjectProperty(":isEmployedAt", pm);
-
-        for (OWLLiteral fluSymptom : reasoner.getDataPropertyValues(flu, includesSymptomProperty)) {
-            System.out.println("Flu includes symptom: " + fluSymptom.getLiteral());
+        // run all individuals
+        for (OWLNamedIndividual person : reasoner.getInstances(patientClass, false).getFlattened()) {
+	        System.out.println(renderer.render(person));
+        	//OWLNamedIndividual person = factory.getOWLNamedIndividual(":Patient0", pm);
+	        // get values of selected properties on the individual
+	        OWLObjectProperty hasDisease = factory.getOWLObjectProperty(":hasDisease", pm);
+	        // put all diseases in a map. 
+	        for (OWLNamedIndividual disease : reasoner.getObjectPropertyValues(person, hasDisease).getFlattened()) {
+	        	diseaseSymptoms.put(renderer.render(disease),0.0);
+	        }
+	
+	        String[] diseases = diseaseSymptoms.keySet().toArray(new String[0]);
+	        
+	        for (OWLObjectProperty objProp : ontology.getObjectPropertiesInSignature(Imports.INCLUDED))
+	            for (OWLNamedIndividual ind : reasoner.getObjectPropertyValues(person, objProp).getFlattened())
+	                for (String disease: diseases)
+	                	if (renderer.render(objProp).toLowerCase().contains(disease.toLowerCase()))
+	                		diseaseSymptoms.put(disease, diseaseSymptoms.get(disease)+1);
+	        
+	        // get max length of diseases + convert symptom count to fraction of total symptoms for disease
+	        int max = 0;
+	        for (String disease : diseaseSymptoms.keySet()) {
+	        	// update max length of diseases if needed
+	        	if (disease.length() > max)
+	        		max = disease.length();
+	        	OWLNamedIndividual curDisease = factory.getOWLNamedIndividual(":"+disease, pm);
+	        	OWLObjectProperty includesSymptom = factory.getOWLObjectProperty(":includesSymptom", pm);
+	        	int numSymptoms = reasoner.getObjectPropertyValues(curDisease, includesSymptom).getFlattened().size();
+	        	diseaseSymptoms.put(disease, diseaseSymptoms.get(disease)/numSymptoms);
+	        }
+	        
+	        System.out.println("The diseases that " + renderer.render(person) + " may have are: (ordered by likeliness)");
+	        for (Map.Entry<String, Double> entry : sortDiseases())
+	        	System.out.println(String.format("\t%"+max+"s\t%.3f", entry.getKey(), entry.getValue()));
         }
-
         /*
         for (OWLNamedIndividual ind : reasoner.getObjectPropertyValues(martin, isEmployedAtProperty).getFlattened()) {
             System.out.println("Martin is employed at: " + renderer.render(ind));
@@ -97,26 +191,26 @@ public class Disease {
         */
 
         //find to which classes the individual belongs
-        Collection<OWLClassExpression> assertedClasses = EntitySearcher.getTypes(flu, ontology);
-        for (OWLClass c : reasoner.getTypes(flu, false).getFlattened()) {
-            boolean asserted = assertedClasses.contains(c);
-            System.out.println((asserted ? "asserted" : "inferred") + " class for Flu: " + renderer.render(c));
-        }
+//        Collection<OWLClassExpression> assertedClasses = EntitySearcher.getTypes(person, ontology);
+//        for (OWLClass c : reasoner.getTypes(person, false).getFlattened()) {
+//            boolean asserted = assertedClasses.contains(c);
+//            System.out.println((asserted ? "asserted" : "inferred") + " class for Flu: " + renderer.render(c));
+//        }
 
-        //list all object property values for the individual
-        Multimap<OWLObjectPropertyExpression, OWLIndividual> assertedValues = EntitySearcher.getObjectPropertyValues(flu, ontology);
-        for (OWLObjectProperty objProp : ontology.getObjectPropertiesInSignature(Imports.INCLUDED)) {
-            for (OWLNamedIndividual ind : reasoner.getObjectPropertyValues(flu, objProp).getFlattened()) {
-                boolean asserted = assertedValues.get(objProp).contains(ind);
-                System.out.println((asserted ? "asserted" : "inferred") + " object property for Flu: "
-                        + renderer.render(objProp) + " -> " + renderer.render(ind));
-            }
-        }
+//        //list all object property values for the individual
+//        Multimap<OWLObjectPropertyExpression, OWLIndividual> assertedValues = EntitySearcher.getObjectPropertyValues(person, ontology);
+//        for (OWLObjectProperty objProp : ontology.getObjectPropertiesInSignature(Imports.INCLUDED)) {
+//            for (OWLNamedIndividual ind : reasoner.getObjectPropertyValues(person, objProp).getFlattened()) {
+//                boolean asserted = assertedValues.get(objProp).contains(ind);
+//                System.out.println((asserted ? "asserted" : "inferred") + " object property for Flu: "
+//                        + renderer.render(objProp) + " -> " + renderer.render(ind));
+//            }
+//        }
 
         //list all same individuals
-        for (OWLNamedIndividual ind : reasoner.getSameIndividuals(flu)) {
-            System.out.println("same as Flu: " + renderer.render(ind));
-        }
+//        for (OWLNamedIndividual ind : reasoner.getSameIndividuals(person)) {
+//            System.out.println("same as Flu: " + renderer.render(ind));
+//        }
 
         /*TODO
         //ask reasoner whether Flu includes symptom Fever
@@ -154,6 +248,7 @@ public class Disease {
         }
     }
 
+    
     /**
      * Helper class for extracting labels, comments and other annotations in preferred languages.
      * Selects the first literal annotation matching the given languages in the given order.
@@ -215,4 +310,3 @@ public class Disease {
         }
     }
 }
-
