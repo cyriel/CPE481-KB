8131051b661ebc0d71a2ec99d2a0c0a7b9fde91a
tramxme <tramlaisf@gmail.com>
2016-05-29 17:38:07 -0700

    Clean up

--- 481DxDz/src/main/java/cz/makub/Disease.java
+++ 481DxDz/src/main/java/cz/makub/Disease.java
@@ -85,34 +85,36 @@ public class Disease {
             symptomDB.put(renderer.render(symptom), symptom);
         }
         
-        // prompt user for disease. Do we ask for multiple people, or just run them once?
-        // should we just keep it single user?
         String[] symptoms =symptomDB.keySet().toArray(new String[0]);
+        Arrays.sort(symptoms);
         selected = new boolean[symptomDB.size()];
         
         int longest = 0;
-        for (String str: symptoms)
-        	if (longest < str.length())
-        		longest = str.length();
-		longest++;
+        for (String str: symptoms){
+            if (longest < str.length()){
+                longest = str.length();
+            }
+            longest++;
+        }
         
         String parse;
         do {
-        	for (int i = 0; i < symptoms.length; i++) {
-        		if ((i != 0 && symptoms.length < 10) || (i % 3 == 0 && i != 0))
-        			System.out.println();
-        		int maxIntLength = (new String(""+symptoms.length)).length();
-        		String index = new String("" + (i+1));
-        		System.out.print("[" + (selected[i]? "x" : " ") + "] " +  String.format("%"+maxIntLength+"s", index) + ". " + symptoms[i].replace("_", " ") +  String.format("%" + (longest-symptoms[i].length()) + "s"," ") + " ");
-        	}
-        	System.out.print("\nPlease select symptoms (press <ENTER> to continue): ");
-        	parse = scan.nextLine().trim();
-        	if (parse.length() == 0) break;
-    		String[] parsed = parse.split(",");
-    		for (int i = 0; i < parsed.length; i++) {
-    			// no type checking
-    			selected[Integer.parseInt(parsed[i].trim())-1] = true;
-    		}
+            for (int i = 0; i < symptoms.length; i++) {
+                if ((i != 0 && symptoms.length < 10) || (i % 3 == 0 && i != 0)){
+                    System.out.println();
+                }
+        	int maxIntLength = (new String(""+symptoms.length)).length();
+        	String index = new String("" + (i+1));
+        	System.out.print("[" + (selected[i]? "x" : " ") + "] " +  String.format("%"+maxIntLength+"s", index) + ". " + symptoms[i].replace("_", " ") +  String.format("%" + (longest-symptoms[i].length()) + "s"," ") + " ");
+            }
+                
+            System.out.print("\nPlease select symptoms (comma separated) (press <ENTER> to continue): ");
+            parse = scan.nextLine().trim();
+            if (parse.length() == 0) break;
+            String[] parsed = parse.split(",");
+            for (int i = 0; i < parsed.length; i++) {
+                selected[Integer.parseInt(parsed[i].trim())-1] = true;
+            }
         } while (true);
         
         OWLNamedIndividual patient0 = factory.getOWLNamedIndividual(":patient0", pm);
@@ -121,127 +123,61 @@ public class Disease {
         OWLObjectProperty hasSymptoms = factory.getOWLObjectProperty(":hasSymptoms", pm);
         
         // transfer symptoms into ontology
-		for (int i = 0; i < symptoms.length; i++) {
-			if (selected[i]) {
-//				System.out.print(symptoms[i]);
-				manager.addAxiom(ontology, factory.getOWLObjectPropertyAssertionAxiom(hasSymptoms, patient0, symptomDB.get(symptoms[i])));
-//				System.out.println();
-			}
-		}
+	for (int i = 0; i < symptoms.length; i++) {
+            if (selected[i]) {
+                manager.addAxiom(ontology, factory.getOWLObjectPropertyAssertionAxiom(hasSymptoms, patient0, symptomDB.get(symptoms[i])));
+            }
+	}
 
-		reasoner = reasonerFactory.createReasoner(ontology, new SimpleConfiguration());
-		
-//        for (OWLNamedIndividual ind : reasoner.getObjectPropertyValues(patient0, hasSymptoms).getFlattened()) {
-//            System.out.println(renderer.render(ind));
-//        }
-		
+	reasoner = reasonerFactory.createReasoner(ontology, new SimpleConfiguration());
+			
         // run all individuals
-		// TODO: clean up strings to sound more user friendly
+	// TODO: clean up strings to sound more user friendly
         for (OWLNamedIndividual person : reasoner.getInstances(patientClass, false).getFlattened()) {
-        	diseaseSymptoms = new HashMap<String, Double>();
-        	//OWLNamedIndividual person = factory.getOWLNamedIndividual(":Patient0", pm);
-	        // get values of selected properties on the individual
-	        OWLObjectProperty hasDisease = factory.getOWLObjectProperty(":hasDisease", pm);
-	        // put all diseases in a map. 
-	        for (OWLNamedIndividual disease : reasoner.getObjectPropertyValues(person, hasDisease).getFlattened()) {
-	        	diseaseSymptoms.put(renderer.render(disease),0.0);
-	        }
+            diseaseSymptoms = new HashMap<String, Double>();
+        	
+	    // get values of selected properties on the individual
+	    OWLObjectProperty hasDisease = factory.getOWLObjectProperty(":hasDisease", pm);
+	    
+            // put all diseases in a map. 
+	    for (OWLNamedIndividual disease : reasoner.getObjectPropertyValues(person, hasDisease).getFlattened()) {
+                diseaseSymptoms.put(renderer.render(disease),0.0);
+            }
 	
-	        String[] diseases = diseaseSymptoms.keySet().toArray(new String[0]);
+	    String[] diseases = diseaseSymptoms.keySet().toArray(new String[0]);
 	        
-	        for (OWLObjectProperty objProp : ontology.getObjectPropertiesInSignature(Imports.INCLUDED))
-	            for (OWLNamedIndividual ind : reasoner.getObjectPropertyValues(person, objProp).getFlattened())
-	                for (String disease: diseases)
-	                	if (renderer.render(objProp).toLowerCase().contains(disease.toLowerCase()))
-	                		diseaseSymptoms.put(disease, diseaseSymptoms.get(disease)+1);
+	    for (OWLObjectProperty objProp : ontology.getObjectPropertiesInSignature(Imports.INCLUDED)){
+	        for (OWLNamedIndividual ind : reasoner.getObjectPropertyValues(person, objProp).getFlattened()){
+	            for (String disease: diseases){
+	                if (renderer.render(objProp).toLowerCase().contains(disease.toLowerCase())){
+                            diseaseSymptoms.put(disease, diseaseSymptoms.get(disease)+1);   
+                        }
+                    }
+                }
+            }	        
+            
+	    // get max length of diseases + convert symptom count to fraction of total symptoms for disease
+	    int max = 0;
+                
+	    for (String disease : diseaseSymptoms.keySet()) {
+	        // update max length of diseases if needed
+	        if (disease.length() > max){
+                    max = disease.length();
+                }
+	        OWLNamedIndividual curDisease = factory.getOWLNamedIndividual(":"+disease, pm);
+	        OWLObjectProperty includesSymptom = factory.getOWLObjectProperty(":includesSymptom", pm);
+                int numSymptoms = reasoner.getObjectPropertyValues(curDisease, includesSymptom).getFlattened().size();
+	        diseaseSymptoms.put(disease, diseaseSymptoms.get(disease)/numSymptoms);
+            }   
 	        
-	        // get max length of diseases + convert symptom count to fraction of total symptoms for disease
-	        int max = 0;
-	        for (String disease : diseaseSymptoms.keySet()) {
-	        	// update max length of diseases if needed
-	        	if (disease.length() > max)
-	        		max = disease.length();
-	        	OWLNamedIndividual curDisease = factory.getOWLNamedIndividual(":"+disease, pm);
-	        	OWLObjectProperty includesSymptom = factory.getOWLObjectProperty(":includesSymptom", pm);
-	        	int numSymptoms = reasoner.getObjectPropertyValues(curDisease, includesSymptom).getFlattened().size();
-	        	diseaseSymptoms.put(disease, diseaseSymptoms.get(disease)/numSymptoms);
-	        }
-	        
-	        System.out.println("The diseases that " + renderer.render(person) + " may have are: (ordered by likeliness)");
-	        for (Map.Entry<String, Double> entry : sortDiseases())
-	        	System.out.println(String.format("\t%"+max+"s\t%.3f", entry.getKey(), entry.getValue()));
+	    System.out.println("The diseases that " + renderer.render(person) + " may have are: (ordered by likeliness)");
+	    for (Map.Entry<String, Double> entry : sortDiseases()){
+	        System.out.println(String.format("\t%"+max+"s\t%.3f", entry.getKey(), entry.getValue()));
+            }
         }
-        /*
-        for (OWLNamedIndividual ind : reasoner.getObjectPropertyValues(martin, isEmployedAtProperty).getFlattened()) {
-            System.out.println("Martin is employed at: " + renderer.render(ind));
-        }
-        */
-
-        /* this is for data properties - not any in our ontology
-        //get labels
-        LocalizedAnnotationSelector as = new LocalizedAnnotationSelector(ontology, factory, "en", "cs");
-        for (OWLNamedIndividual ind : reasoner.getObjectPropertyValues(flu, includesSymptomProperty).getFlattened()) {
-            System.out.println("Flu includes symptom (label): '" + as.getLabel(ind) + "'");
-        }
-        */
-
-        /*
-        //get inverse of a property, i.e. which individuals are in relation with a given individual
-        OWLNamedIndividual university = factory.getOWLNamedIndividual(":MU", pm);
-        OWLObjectPropertyExpression inverse = factory.getOWLObjectInverseOf(isEmployedAtProperty);
-        for (OWLNamedIndividual ind : reasoner.getObjectPropertyValues(university, inverse).getFlattened()) {
-            System.out.println("MU inverseOf(isEmployedAt) -> " + renderer.render(ind));
-        }
-        */
-
-        //find to which classes the individual belongs
-//        Collection<OWLClassExpression> assertedClasses = EntitySearcher.getTypes(person, ontology);
-//        for (OWLClass c : reasoner.getTypes(person, false).getFlattened()) {
-//            boolean asserted = assertedClasses.contains(c);
-//            System.out.println((asserted ? "asserted" : "inferred") + " class for Flu: " + renderer.render(c));
-//        }
-
-//        //list all object property values for the individual
-//        Multimap<OWLObjectPropertyExpression, OWLIndividual> assertedValues = EntitySearcher.getObjectPropertyValues(person, ontology);
-//        for (OWLObjectProperty objProp : ontology.getObjectPropertiesInSignature(Imports.INCLUDED)) {
-//            for (OWLNamedIndividual ind : reasoner.getObjectPropertyValues(person, objProp).getFlattened()) {
-//                boolean asserted = assertedValues.get(objProp).contains(ind);
-//                System.out.println((asserted ? "asserted" : "inferred") + " object property for Flu: "
-//                        + renderer.render(objProp) + " -> " + renderer.render(ind));
-//            }
-//        }
-
-        //list all same individuals
-//        for (OWLNamedIndividual ind : reasoner.getSameIndividuals(person)) {
-//            System.out.println("same as Flu: " + renderer.render(ind));
-//        }
-
-        /*TODO
-        //ask reasoner whether Flu includes symptom Fever
-        boolean result = reasoner.isEntailed(factory.getOWLObjectPropertyAssertionAxiom(includesSymptomProperty, flu, university));
-        System.out.println("Is Martin employed at MU ? : " + result);
-
-
-        //check whether the SWRL rule is used
-        OWLNamedIndividual ivan = factory.getOWLNamedIndividual(":Ivan", pm);
-        OWLClass chOMPClass = factory.getOWLClass(":ChildOfMarriedParents", pm);
-        OWLClassAssertionAxiom axiomToExplain = factory.getOWLClassAssertionAxiom(chOMPClass, ivan);
-        System.out.println("Is Ivan child of married parents ? : " + reasoner.isEntailed(axiomToExplain));
-
-
-        //explain why Ivan is child of married parents
-        DefaultExplanationGenerator explanationGenerator =
-                new DefaultExplanationGenerator(
-                        manager, reasonerFactory, ontology, reasoner, new SilentExplanationProgressMonitor());
-        Set<OWLAxiom> explanation = explanationGenerator.getExplanation(axiomToExplain);
-        ExplanationOrderer deo = new ExplanationOrdererImpl(manager);
-        ExplanationTree explanationTree = deo.getOrderedExplanation(axiomToExplain, explanation);
-        System.out.println();
-        System.out.println("-- explanation why Ivan is in class ChildOfMarriedParents --");
-        printIndented(explanationTree, "");
-        */
     }
 
+    /* This method prints the diagnosis */
     private static void printIndented(Tree<OWLAxiom> node, String indent) {
         OWLAxiom axiom = node.getUserObject();
         System.out.println(indent + renderer.render(axiom));
